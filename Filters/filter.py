bf529d8ff0c1afb8c0c01d32cda1302ba89d3153
Nathan Hadfield <hadfieldn@gmail.com>
2014-05-21 13:29:34 -0600

    Modified algorithm for creating sample table -- unfortunately, not working -- getting sample impoverishment.

--- Filters/filter.py
+++ Filters/filter.py
@@ -10,25 +10,31 @@ if __name__ == '__main__':
 
     file = open("output.txt", "w")
 
-    actual_x = random.normalvariate(0, 1)
-    actual_y = random.normalvariate(0, 1)
-    current_table = Table.initialized_with_start_position(100, actual_x, actual_y)
-    model = model.RobotModel(10)                # (nh) gives robot coordinates
-    #model = model.BeaconModel(10)              # (nh) gives beacon coordinates (what I think we need to use)
-    #model = model.FileModel("test_data.txt")   # (nh) gives beacon coordinates (test data from Dr. Seppi)
-    for r_a, r_b in model.coordinates():
-        log.debug("Coordinate: ({}, {})".format(r_a, r_b))
-        current_table.assign_weights(r_a, r_b)
-        next_table = current_table.create_sample_table()
+    current_table = Table.initialized_with_start_position(1000, 0, 0)
+    log.debug("Initial sample table:\n{}".format(current_table))
 
-        if log.is_debug_enabled():
-            log.debug("New Weight table:\n{}".format(next_table))
+    #model = model.RobotModel(10)                # (nh) gives robot coordinates
+    #model = model.BeaconModel(10)              # (nh) gives beacon coordinates (what I think we need to use)
+    model = model.FileModel("test_data.txt")   # (nh) gives beacon coordinates (test data from Dr. Seppi)
+    #model = model.KalmanSampleDataModel("kalman_sample_data.txt")
+
+    for r_a, r_b in model.coordinates():
+
+        log.debug("Obtained beacon coordinates ({}, {}), determining likely robot position...".format(r_a, r_b))
+
+        current_table.assign_weights(r_a, r_b)
+        log.debug("Weighted samples:\n{}\n".format(current_table))
+
+        next_table = current_table.create_sample_table()
+        log.debug("New sample table:\n{}\n".format(next_table))
 
         status = next_table.status_string_for_output()
-        file.write(status)
+        file.write(status + "\n")
         print(status)
 
         next_table.transition()
+        log.debug("After transitioning:\n{}".format(next_table))
+
         current_table = next_table
     file.close()
 
